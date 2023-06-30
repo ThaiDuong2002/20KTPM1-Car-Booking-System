ac318a691564717db3d1b4762a29dc1e60c65521
Minh Tri Nguyen Vo <minhtri.nguyenvo31@gmail.com>
2023-06-30 18:16:02 +0700

    set up admin db

--- Client/Web/Admin/src/index.js
+++ Client/Web/Admin/src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createRoot } from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// third party
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+
+// project imports
+
+import App from "./App";
+import { store } from "./store";
+
+// style + assets
+import "./assets/scss/_themes-vars.module.scss";
+import config from "./config";
+
+// ==============================|| REACT DOM RENDER  ||============================== //
+
+const container = document.getElementById("root");
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <BrowserRouter basename={config.basename}>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ Client/Web/Admin/src/routes/MainRoutes.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+import MainLayout from "../layout/MainLayout/MainLayout";
+
+const MainRoutes = {
+  path: "/",
+  element: <MainLayout />,
+};
+
+export default MainRoutes;
