f4ec4f51721682e6df121138711610ee41c5dde7
Minh Tri Nguyen Vo <minhtri.nguyenvo31@gmail.com>
2023-06-30 18:24:38 +0700

    config set up admin dasboard

---
+++ Client/Web/Admin/src/routes/AuthenticationRoutes.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+import AnthenticationLayout from "../layout/AuthenticationLayout";
+
+const AuthencationRoutes = {
+  path: "/authen",
+  element: <AnthenticationLayout />,
+};
+
+export default AuthencationRoutes;
